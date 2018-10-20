d312d5df08420bd97c651506c89b6dc4335f59a9
VolodymyrKuksa <vkuksa@student.unit.ua>
2018-10-20 18:34:03 +0300

    hypens are now ignored when searching for isograms

--- Volodymyr_Kuksa/1/Isogram.py
+++ Volodymyr_Kuksa/1/Isogram.py
@@ -3,7 +3,7 @@ input_string = input('input a string to determine if it is an isogram: ')
 input_string = input_string.casefold()
 
 for character in input_string:
-    if character.isspace():
+    if character.isspace() or character == '-':
         continue
 
     if input_string.count(character) != 1:
