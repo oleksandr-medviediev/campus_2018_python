0979d033bd076f0f40a676eedeb3d1e2c1580773
Tihran Katolikian <moreggmoreskill@icloud.com>
2018-10-21 21:52:52 +0300

    Solve Get the Middle Character

---
+++ Tihran_Katolikian/GettheMiddleCharacter.py
@@ -0,0 +1,14 @@
+#for floor
+import math
+
+s = input('Enter a string: ')
+
+if len(s) == 0:
+    #nothing to print
+    pass
+else:
+    if len(s) % 2 == 0:
+        rightMiddleIndex = int(len(s) / 2)
+        print(s[rightMiddleIndex - 1:rightMiddleIndex + 1])
+    else:
+        print(s[math.floor(len(s) / 2)])
