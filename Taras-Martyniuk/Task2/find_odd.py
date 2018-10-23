2a059f0bf0eb28cf14a6fee809fc1a42d4524aff
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-10-23 23:37:59 +0300

    find odd

---
+++ Taras-Martyniuk/Task2/find_odd.py
@@ -0,0 +1,18 @@
+def find_odd(numbers):
+
+    """
+        :param numbers: iterable of ints
+        :returns: first odd int in the numbers or None
+    """
+
+    # i think, if i first evaluated the comprehention below as list comprehention,
+    # it would filter the whole collection
+    # this way though, it should stop when the first valid elem is found
+    first_odd = next((x for x in numbers if x % 2 == 1), None)
+    return first_odd
+
+with_odd = [2, 4, 6, 8, 1, 10]
+assert find_odd(with_odd) == 1
+
+without_odd = [42, 42, 42]
+assert find_odd(without_odd) is None
