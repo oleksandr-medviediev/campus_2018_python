7c4ee5ca9eb8f0d72316ff9ab0b49638a40a33b0
Tihran Katolikian <moreggmoreskill@icloud.com>
2018-10-24 22:31:45 +0300

    Fix CustomMap

---
+++ Tihran_Katolikian/2/CustomMap.py
@@ -0,0 +1,20 @@
+def custom_map(func, *iterables):
+
+    """
+    def custom_map(func, *iterables)
+    Invokes func passing as arguments tuples made from *iterables argument.
+
+    :param func: a function which expects len(*iterables) number of arguments
+    :param *iterables: any number of iterables
+    :return: list filled with results returned by func argument. length of list
+    will be equal to a length of the shortest iterable argument
+    :rtype: list
+    """
+
+    results = [ func(*args) for args in zip(*iterables) ]
+    return results
+
+
+find_sum = lambda x, y: x + y
+sum_of_vectors = custom_map(find_sum, [1, 2, 1], [-1, -2, -1])
+print(sum_of_vectors)
