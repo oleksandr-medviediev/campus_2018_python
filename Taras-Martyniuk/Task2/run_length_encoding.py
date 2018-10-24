0a4b789df175561c82d56b7568e5b77d9863cc87
Taras Martyniuk <taras.martyniuk@ubisoft.com>
2018-10-24 12:50:44 +0300

    run length encode

--- Taras-Martyniuk/Task2/run_length_encoding.py
+++ Taras-Martyniuk/Task2/run_length_encoding.py
@@ -17,7 +17,9 @@ def run_length_encode(string):
     return "".join(group_str_list)
 
 def pairwise(iterable):
+
     "s -> (s0, s1), (s2, s3), (s4, s5), ..."
+
     a = iter(iterable)
     return zip(a, a)
 
@@ -31,25 +33,19 @@ def decode(string):
     res = []
 
     char_digits_groups = groupby(string, lambda x : x.isdigit())
+    char_times_strs = [ "".join(list(group)) for _, group in char_digits_groups ]
 
-    for char_pair, digit_pair in char_digits_groups:
-        print(char_pair[0])
-        print(list(char_pair[1]))
+    for char, times_encountered_str in pairwise(char_times_strs):
+        assert times_encountered_str.isdigit()
 
-        print(digit_pair[0])
-        print(list(digit_pair[1]))
-
-    # for i in range(0, len(string), 2):
-    #     char = string[i]
-    #     times_present = int(string[i + 1])
-    #     res.append(char * times_present)
+        times_encountered = int(times_encountered_str)
+        res.append(char * times_encountered)
 
     return "".join(res)
 
 original = "H" * 10 + "W" * 20 + "H" * 10
 encoded = "H10W20H10"
 assert run_length_encode(original) == encoded
-
-print(decode(encoded))
+assert decode(encoded) == original
 
 
