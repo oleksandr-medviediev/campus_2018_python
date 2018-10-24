f34f990f3d2dfde3b357183d636d881f6235965d
Taras Martyniuk <taras.martyniuk@ubisoft.com>
2018-10-24 13:39:57 +0300

    armstrong

---
+++ Taras-Martyniuk/Task2/armstrong.py
@@ -0,0 +1,23 @@
+def armstrong(number):
+
+    """
+        :param number: int
+        :returns: sum of all digits of number to power of number of all digits
+    """
+
+    digits = [int(x) for x in str(number)]
+    powers = [x ** len(digits) for x in digits]
+    return sum(powers)
+
+def is_armstrong(number):
+
+    """
+        :param number: int
+        :returns: bool - is it an armstrong number?
+    """
+
+    return number == armstrong(number)
+
+
+assert is_armstrong(153)
+assert not is_armstrong(10)
