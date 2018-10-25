eaa29716f447c702180b7b5e91195bb94818fbfe
Shalim23 <31928965+Shalim23@users.noreply.github.com>
2018-10-25 23:16:23 +0300

    Update 6.py

--- Dmytro_Shalimov/2/6.py
+++ Dmytro_Shalimov/2/6.py
@@ -1,7 +1,7 @@
 print("Given a person's allergy score, determine whether or not they're allergic to a given item, and their full list of allergies.")
 
-allergies = {'1': "eggs", '2': "peanuts", '4': "shellfish",
- '8': "strawberries", '16': "tomatoes", '32': "chocolate", '64': "pollen", '128': "cats"}
+allergies = {1: "eggs", 2: "peanuts", 4: "shellfish",
+ 8: "strawberries", 16: "tomatoes", 32: "chocolate", 64: "pollen", 128: "cats"}
 
 max_allergen = 128
 
@@ -15,7 +15,7 @@ def process_allergies(arg, next_allergen, determined_allergies):
 
     elif arg // next_allergen == 1:
 
-        new_determined_allergies.append(allergies.get(str(next_allergen)))
+        new_determined_allergies.append(allergies.get(next_allergen))
 
         return process_allergies(arg % next_allergen, next_allergen // 2, determined_allergies)
 
