fbabf8c66615ee316205fb78d67fa9933ed4beb6
VolodymyrKuksa <vkuksa@student.unit.ua>
2018-10-28 12:12:46 +0200

    added docstrings for rock_paper_scissors.py

--- Volodymyr_Kuksa/3/rock_paper_scissors.py
+++ Volodymyr_Kuksa/3/rock_paper_scissors.py
@@ -7,8 +7,13 @@ NEGATIVE_ANSWERS = ('n', 'no')
 
 
 def query_number_of_players():
+    """
+    Query input that is a number from 2 to 5.
 
-    number_of_players = input('Enter number of players (2-5): ')
+    :return: entered number.
+    :rtype: int.
+    """
+    number_of_players = input('\nEnter number of players (2-5): ')
 
     while not number_of_players.isdigit() or not 2 <= int(number_of_players) <= 5:
         number_of_players = input('Wrong! Try again: ')
@@ -17,8 +22,13 @@ def query_number_of_players():
 
 
 def query_player_weapon():
+    """
+    Query input that represents a weapon (rock, paper, scissors).
 
-    weapon = input('Enter your weapon (rock, paper, scissors): ')
+    :return: entered weapon.
+    :rtype: str.
+    """
+    weapon = input('\nEnter your weapon (rock, paper, scissors): ')
 
     while not AVAILABLE_WEAPONS.count(weapon):
         weapon = input('Wrong! Try again: ')
@@ -26,10 +36,16 @@ def query_player_weapon():
     return weapon
 
 
-def query_weapon(name):
+def choose_weapons(name):
+    """
+    Choose/Query weapons for a participant of the game.
 
-    weapon = ''
+    :param name: name of the participant.
+    :type name: str.
 
+    :return: name of the weapon.
+    :rtype: str.
+    """
     if name == 'You':
         weapon = query_player_weapon()
     else:
@@ -39,7 +55,15 @@ def query_weapon(name):
 
 
 def generate_player_names(num_of_players):
+    """
+    Generate list of player names.
 
+    :param num_of_players: number of players.
+    :type num_of_players: int.
+
+    :return: list of player names.
+    :rtype: list.
+    """
     player_names = ['You']
 
     for i in range(1, num_of_players):
@@ -49,7 +73,15 @@ def generate_player_names(num_of_players):
 
 
 def determine_weakest_weapon_for_round(weapons_presence):
+    """
+    Return the name of the weakest weapon in the round.
 
+    :param weapons_presence: dict that determines if the weapon is present in current round.
+    :type weapons_presence: dict(weapon_name: bool).
+
+    :return: name of the weakest weapon in the round.
+    :rtype: str.
+    """
     result = ''
 
     if weapons_presence['rock'] and weapons_presence['paper'] and not weapons_presence['scissors']:
@@ -62,9 +94,17 @@ def determine_weakest_weapon_for_round(weapons_presence):
     return result
 
 
-def determine_loosers(weapons_for_round):
+def determine_losers(weapons_for_round):
+    """
+    Return list of players hwo lost in the current round.
 
-    loosers = []
+    :param weapons_for_round: dict that represents the choice of weapon by each player.
+    :type weapons_for_round: dict(player_name: weapon_name).
+
+    :return: list of players who lost in the current round.
+    :rtype: list.
+    """
+    losers = []
 
     weapons_selected = list(weapons_for_round.values())
 
@@ -75,42 +115,103 @@ def determine_loosers(weapons_for_round):
     for player in weapons_for_round.keys():
 
         if weapons_for_round[player] == weakest_weapon:
-            loosers.append(player)
+            losers.append(player)
 
-    return loosers
+    return losers
+
+
+def display_choice_of_weapons(weapons_for_round):
+    """
+    Display choice of weapons for each player in game.
+
+    :param weapons_for_round: dict of player names and their weapons.
+    :type weapons_for_round: dict(player_name: weapon_name).
+
+    :return: None
+    """
+    names = tuple(weapons_for_round.keys())
+    weapons = tuple(weapons_for_round.values())
+
+    print('\nWeapon of the round:')
+
+    for i in range(len(names)):
+        print(f'{names[i]} chose {weapons[i]}.')
+
+
+def display_losers(losers):
+    """
+    Display all losers for the round.
+
+    :param losers: list of losers.
+    :type losers: list.
+
+    :return: None.
+    """
+    if not len(losers):
+        print('\nDRAW!')
+    else:
+        print('\nLosers of the round:')
+
+    for loser in losers:
+        print(f'{loser} dropped out.')
+
+
+def display_leader_board(rank):
+    """
+    Display leader board.
+
+    :param rank: list of player names, sorted in descending order according to their rank.
+    :type rank: iterable
+
+    :return: None
+    """
+    print('\nLeader Board:')
+
+    place = 1
+
+    for player in rank[::-1]:
+
+        print(f'{place}. {player}')
+        place += 1
 
 
 def run_game():
+    """
+    Execute game logic until there is only one player left.
 
+    :return: None.
+    """
     num_of_players = query_number_of_players()
 
     active_players = generate_player_names(num_of_players)
-    rank = {}
+    rank = []
 
     while len(active_players) > 1:
 
-        weapons_for_round = {player: query_weapon(player) for player in active_players}
-        print(weapons_for_round)
+        weapons_for_round = {player: choose_weapons(player) for player in active_players}
+        display_choice_of_weapons(weapons_for_round)
 
-        loosers = determine_loosers(weapons_for_round)
-        print(loosers)
+        losers = determine_losers(weapons_for_round)
+        display_losers(losers)
 
-        for player in active_players:
+        for loser in losers:
 
-            if loosers.count(player):
+            rank.append(loser)
+            active_players.remove(loser)
 
-                rank[player] = len(active_players)
-                active_players.remove(player)
+    rank.extend(active_players)
 
-    rank[active_players[0]] = 1
-
-    print('leaderboard:')
-    print(rank)
+    display_leader_board(rank)
 
 
 def query_replay():
+    """
+    Query the player to play again.
 
-    answer = input('Play again? (Y/N): ').casefold()
+    :return: True if the player wants to play again, False otherwise.
+    :rtype: bool.
+    """
+    answer = input('\nPlay again? (Y/N): ').casefold()
 
     while not POSITIVE_ANSWERS.count(answer) and not NEGATIVE_ANSWERS.count(answer):
         answer = input('Wrong! Try again: ').casefold()
@@ -119,7 +220,11 @@ def query_replay():
 
 
 def play_rock_paper_scissors():
+    """
+    Run games of rock paper scissors as long as player wants to.
 
+    :return: None.
+    """
     while True:
 
         run_game()
