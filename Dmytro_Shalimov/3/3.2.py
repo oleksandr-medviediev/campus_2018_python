fcdd01b9b92425db8050a4360ef35ae8ff82f155
Shalim23 <31928965+Shalim23@users.noreply.github.com>
2018-10-29 09:00:05 +0200

    3.2 resolved

--- Dmytro_Shalimov/3/3.2.py
+++ Dmytro_Shalimov/3/3.2.py
@@ -16,7 +16,7 @@ def normalize_string(string):
     return normalized_string
 
 
-def define_rows_and_columns(string_len):
+def define_rows_and_columns_for_encode(string_len):
     """
     Counts rows and colums for encoding given string
 
@@ -43,9 +43,9 @@ def define_rows_and_columns(string_len):
     return (rows, colums)
 
 
-def split_for_encryption(string, rows_and_colums):
+def split_for_encode(string, rows_and_colums):
     """
-    Splits given string on parts using given rows and columns amounts
+    Splits given string to encode on parts using given rows and columns amounts
 
     :param str string: normalized string
     :param tuple rows_and_columns: tuple with row as first element and column as second
@@ -89,8 +89,8 @@ def encode(normalized_string):
     :rtype: str
     """
 
-    rows_and_columns = define_rows_and_columns(len(normalized_string))
-    splitted_text = split_for_encryption(normalized_string, rows_and_columns)
+    rows_and_columns = define_rows_and_columns_for_encode(len(normalized_string))
+    splitted_text = split_for_encode(normalized_string, rows_and_columns)
 
     output = ''
     column_counter = 0
@@ -119,8 +119,89 @@ def encode(normalized_string):
     return output
 
 
-user_input = input("Enter a string to encrypt: ")
+def define_rows_and_columns_for_decode(string_len):
+    """
+    Counts rows and colums for decoding given string
 
+    :param int string_len: length of string to encode
+    :return: tuple with row as first element and column as second
+    :rtype: tuple
+    """
+
+    rows = string_len
+    colums = 0
+
+    current_num = rows
+
+    while rows - colums > 1:
+        
+        if current_num ** 2 > string_len:
+            rows = current_num
+
+        else:
+            colums = current_num
+
+        current_num = (colums + rows) // 2
+
+    return (rows, colums)
+
+
+def split_for_decode(string, rows_and_colums):
+    """
+    Splits given string to decode on parts using given rows and columns amounts
+
+    :param str string: normalized string
+    :param tuple rows_and_columns: tuple with row as first element and column as second
+    :return: list of splitted string to encode
+    :rtype: list
+    """
+
+    splitted_text = []
+    left_len = len(string)
+
+    columns = rows_and_colums[-1]
+
+    next_start_position = 0
+    next_end_position = 0
+
+    while left_len != 0:
+
+        if next_end_position + columns > len(string):
+
+            next_end_position += left_len
+            left_len = 0
+
+        else:
+            next_end_position += columns
+            left_len -= columns
+
+        text_row = string[next_start_position : next_end_position]
+        splitted_text.append(text_row)
+
+        next_start_position += columns
+
+    return splitted_text
+
+
+def decode(encoded_string):
+    """
+    Decodes given string using square code crypto method
+
+    :param str encoded_string: encoded string to decode
+    :return: decoded string
+    :rtype: str
+    """
+
+    rows_and_columns = define_rows_and_columns_for_decode(len(normalized_string))
+    splitted_text = split_for_decode(normalized_string, rows_and_columns)
+
+    return ''.join(splitted_text)
+
+
+user_input = input("Enter a string to encrypt: ")
 normalized_string = normalize_string(user_input)
 
-print(encode(normalized_string))
+encoded_string = encode(normalized_string)
+
+print("Encoded: ", encoded_string)
+print("Decoded: ", decode(encoded_string))
