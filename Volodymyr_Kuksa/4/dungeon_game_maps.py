45ba75ca871367dc58874ee15647cb300ccf5930
VolodymyrKuksa <vkuksa@student.unit.ua>
2018-10-29 23:46:05 +0200

    added docstrings for dungeon_game_maps.py

--- Volodymyr_Kuksa/4/dungeon_game_maps.py
+++ Volodymyr_Kuksa/4/dungeon_game_maps.py
@@ -5,21 +5,24 @@ GAME_CHARACTERS = {'Empty': ' ', 'Trap': 'X', 'Treasure': 'O'}
 
 
 def generate_flat_map(size_total):
+    """
+    Return an array of randomly generated characters that represent a sequence of all rows in game map.
 
+    :param size_total: the total size of game map.
+    :type size_total: int.
+
+    :return: an array of randomly generated characters that represent a sequence of all rows in game map.
+    :rtype: list.
+    """
     treasure_quantity = size_total // 20
     traps_quantity = size_total // 10
     empty_quantity = size_total - (treasure_quantity + traps_quantity)
 
     characters = []
 
-    for i in range(size_total):
-
-        if i < empty_quantity:
-            characters.append(GAME_CHARACTERS['Empty'])
-        elif i < empty_quantity + traps_quantity:
-            characters.append(GAME_CHARACTERS['Trap'])
-        else:
-            characters.append(GAME_CHARACTERS['Treasure'])
+    characters.extend([GAME_CHARACTERS['Empty']] * empty_quantity)
+    characters.extend([GAME_CHARACTERS['Trap']] * traps_quantity)
+    characters.extend([GAME_CHARACTERS['Treasure']] * treasure_quantity)
 
     shuffle(characters)
 
@@ -27,7 +30,18 @@ def generate_flat_map(size_total):
 
 
 def split_rows(size, characters):
+    """
+    Return an array of batches of characters of size length each.
 
+    :param size: length of one batch.
+    :type size: int.
+
+    :param characters: characters that are split into batches.
+    :type characters: iterable.
+
+    :return: array of batches of characters of size length each.
+    :rtype: list.
+    """
     game_map = []
 
     for i in range(size):
@@ -38,7 +52,15 @@ def split_rows(size, characters):
 
 
 def generate_map(size):
+    """
+    Return square game map of dimensions size x size filled with randomly placed objects.
 
+    :param size: size of map side.
+    :type size: int.
+
+    :return: game map.
+    :rtype: list.
+    """
     characters = generate_flat_map(size * size)
     game_map = split_rows(size, characters)
 
@@ -46,7 +68,14 @@ def generate_map(size):
 
 
 def print_map(game_map):
+    """
+    Print game_map with borders.
 
+    :param game_map: map to print.
+    :type game_map: 2d list of one-character strings.
+
+    :return: None
+    """
     horizontal_border = ''.join(['+', '-' * len(game_map[0]), '+'])
 
     print(horizontal_border)
@@ -58,6 +87,6 @@ def print_map(game_map):
 
 
 if __name__ == '__main__':
-
+    
     new_map = generate_map(randint(5, 10))
     print_map(new_map)
