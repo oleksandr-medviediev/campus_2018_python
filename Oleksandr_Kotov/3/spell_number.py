c064e34d405fc46efdf43ae416167fa45e241658
okotov <oleksandr.kotov@ubisoft.com>
2018-10-31 14:37:57 +0200

    task 1

---
+++ Oleksandr_Kotov/3/spell_number.py
@@ -0,0 +1,79 @@
+NUMBERS = {
+    1 : "one",
+    2 : "two",
+    3 : "three",
+    4 : "four",
+    5 : "five",
+    6 : "six",
+    7 : "seven",
+    8 : "eight",
+    9 : "nine",
+    10 : "ten",
+    11 : "eleven",
+    12 : "twelve",
+    13 : "thirteen",
+    14 : "fourteen",
+    15 : "fifteen",
+    16 : "sixteen",
+    17 : "seventeen",
+    18 : "eighteen",
+    19 : "nineteen",
+    20 : "twenty",
+    30 : "thirty",
+    40 : "fourty",
+    50 : "fifty",
+    60 : "sixty",
+    70 : "seventy",
+    80 : "eighty",
+    90 : "ninety",
+    100 : "hundred",
+    1000 : "thousand",
+    1000000 : "million",
+    1000000000 : "billion",
+}
+
+def spell_hundreds(number):
+
+    spelled_number = ""
+
+    hundreds = number // 100
+
+    if hundreds != 0:
+        spelled_number += NUMBERS[hundreds] + ' ' + NUMBERS[100]
+
+    number %= 100
+
+    if number > 20:
+        
+        spelled_number += ' ' + NUMBERS[number - (number % 10)]
+
+        if number % 10 > 0:
+            spelled_number += '-' + NUMBERS[number % 10]
+
+    elif number > 0:   
+        spelled_number += ' ' + NUMBERS[number]
+
+    return spelled_number
+
+
+def spell_number(number):
+
+    spelled_number = ""
+
+    divider = 1000000000
+
+    while number != 0:
+
+        hundreds = number // divider
+
+        if hundreds != 0:
+
+            spelled_number += ' ' + spell_hundreds(hundreds)
+
+            if divider != 1:
+                spelled_number += ' ' + NUMBERS[divider]
+
+        number %= divider
+        divider /= 1000
+
+    return spelled_number
