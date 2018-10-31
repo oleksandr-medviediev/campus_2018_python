fc063683aa62bd9f2c5fc9debf848bd30214ef1a
knifantiev <kostiantyn.nifantiev@ubisoft.com>
2018-10-31 11:16:32 +0200

    Added function for rows Splitting input string into nested list brought into separate function

--- Kostiantyn_Nifantiev/02/Matrix/MatrixProcessingFunc.py
+++ Kostiantyn_Nifantiev/02/Matrix/MatrixProcessingFunc.py
@@ -7,18 +7,33 @@ def get_matrix():
     return input_matrix
 
 
+def split_matrix(matrix):
+    """
+    Splitting matrix into nested list
+    """
+    result = [row.split(',') for row in matrix.split('\n')]
+
+    return result
+
+
 def read_matrix_rows(matrix):
-    
-    return matrix
+    """
+    Making proper output string throug gluing splitted matrix back
+    Makes rows
+    """
+    splitted_matrix = split_matrix(matrix)
+    rows = [row for row in splitted_matrix]
+    result = '\n'.join([','.join(row) for row in rows])
+
+    return result
 
 
 def read_matrix_columns(matrix):
     """
-    Making proper output string through
-    splitting matrix int 2D list and further gluing it back
+    Making proper output string throug gluing splitted matrix back
+    Makes columns
     """
-    splitted_matrix = [row.split(',') for row in matrix.split('\n')]
-
+    splitted_matrix = split_matrix(matrix)
     columns = [column for column in zip(*splitted_matrix)]
     result = '\n'.join([','.join(column) for column in columns])
 
