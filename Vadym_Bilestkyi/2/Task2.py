8917bb664fc2b380df094554df004cbb97b415fb
vbiletskyi <vbiletskyi@KIV-WKS-PR316>
2018-11-02 16:30:19 +0200

    Array difference.

---
+++ Vadym_Bilestkyi/2/Task2.py
@@ -0,0 +1,19 @@
+def array_diff1(array1, array2):
+    result = array1[:]
+    for element in array2:
+        result.remove(element)
+
+    return result
+
+
+def array_diff2(array1, array2):
+    result = []
+    for element in array1:
+        if element not in array2:
+            result.append(element)
+
+    return result
+
+
+print(array_diff1([1, 2, 2, 2, 3], [2]))
+print(array_diff2([1, 2, 2, 2, 3], [2]))
