25a45b1346b33517d70bf1b28d0759d8a61c81ed
vbiletskyi <vbiletskyi@KIV-WKS-PR316>
2018-11-02 14:56:11 +0200

    Get the Middle Character.

---
+++ Vadym_Bilestkyi/1/Task5.py
@@ -0,0 +1,12 @@
+def get_middle(word):
+    word_len = len(word)
+    is_even = (word_len % 2 == 0)
+
+    middle_start = word_len//2 - (1 if is_even else 0)
+    middle_end = word_len//2 + 1
+
+    return word[middle_start:middle_end]
+
+
+print(get_middle('test'))
+print(get_middle('testing'))
