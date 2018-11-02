0542c83205fdbc8ec3b45dcacdf9edf096d579f7
vbiletskyi <vbiletskyi@KIV-WKS-PR316>
2018-11-02 18:05:14 +0200

    [Fixes] Using spaces around operators.

--- Vadym_Bilestkyi/1/Task5.py
+++ Vadym_Bilestkyi/1/Task5.py
@@ -2,8 +2,8 @@ def get_middle(word):
     word_len = len(word)
     is_even = (word_len % 2 == 0)
 
-    middle_start = word_len//2 - (1 if is_even else 0)
-    middle_end = word_len//2 + 1
+    middle_start = word_len // 2 - (1 if is_even else 0)
+    middle_end = word_len // 2 + 1
 
     return word[middle_start:middle_end]
 
