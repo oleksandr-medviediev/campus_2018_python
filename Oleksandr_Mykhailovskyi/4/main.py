7bcb89ca1b75fcef5ded607a8bfc2cf684f912d3
Oleksandr Mykhailovskyi <oleksandr.mykhailovskyi@ubisoft.com>
2018-11-03 18:59:10 +0200

    Added loading functionality

--- Oleksandr_Mykhailovskyi/4/main.py
+++ Oleksandr_Mykhailovskyi/4/main.py
@@ -2,6 +2,7 @@ import string
 
 import map_generator
 import game_utilities
+import savegame_utility
 from game_utilities import game_step
 
 
@@ -10,6 +11,13 @@ def main():
 
     while True:
         game_map = game_utilities.game_setup()
+        input_action = input("Load game?Y/N\n")
+        input_action = input_action.casefold()
+
+        if(input_action == "Y".casefold()):
+            game_map, game_utilities.position = savegame_utility.load()
+            game_utilities.logger.info("game loaded")
+            print("game loaded")
 
         result = True
         while result:
@@ -18,7 +26,7 @@ def main():
         input_action = input("Continue?Y/N\n")
         input_action = input_action.casefold()
 
-        if input_action.count("N".casefold()) == 1:
+        if input_action == "N".casefold():
             return
 
 
