d5770521d0e4a9840caaaa6ee93fc1b3f4735552
umpire-3 <umpire333@gmail.com>
2018-11-04 20:32:35 +0200

    Verify brackets small fix.

--- Vadym_Bilestkyi/3/Task5.py
+++ Vadym_Bilestkyi/3/Task5.py
@@ -8,9 +8,9 @@ closing_to_opening_map = {
 def verify_brackets(string):
     stack = []
     for letter in string:
-        if letter in ('{', '(', '['):
+        if letter in closing_to_opening_map.values():
             stack.append(letter)
-        elif letter in ('}', ')', ']'):
+        elif letter in closing_to_opening_map.keys():
             if len(stack) == 0 or stack.pop() != closing_to_opening_map[letter]:
                 return False
 
