b847f5662ef6d4cc031dba217c5bcc8500720fa8
afokin <andrii.fokin@ubisoft.com>
2018-11-06 11:09:51 +0200

    map genarator

--- Andrii_Fokin/4/1 Dungeon Game/game_map.py
+++ Andrii_Fokin/4/1 Dungeon Game/game_map.py
@@ -1,5 +1,29 @@
-import random
+from random import randint as rint
+
+
+game_map_size = 10
+cell_free = 0
+cell_trap = -1
+cell_treasure = 1
+
+
+def fill_free_cell(game_map, cell_type):
+    while (True):
+        i = rint(0, game_map_size - 1)
+        j = rint(0, game_map_size - 1)
+
+        if game_map[i][j] == cell_free:
+            game_map[i][j] == cell_type
+            break
+
+
 
 def new_map():
-    return 1
-
+    game_map = [[cell_free for j in range(game_map_size)] for i in range(game_map_size)]
+    
+    for i in range((game_map_size**2) / 20):
+        fill_free_cell(game_map, cell_trap)
+        fill_free_cell(game_map, cell_trap)
+        fill_free_cell(game_map, cell_treasure)
+    
+    return game_map
