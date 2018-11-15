ed7fce351cd9160bb735a43863018c82cd10f8bb
yurii.smazhnyi <yura.smazhny@gmail.com>
2018-11-15 17:25:43 +0200

    Revert "logging is now used through decorators"
    This reverts commit f1534c975408bc5fb4427db4b9c1111d6d25a487.

--- Yurii_Smazhnyi/3/DungeonGame/custom_log.py
+++ Yurii_Smazhnyi/3/DungeonGame/custom_log.py
@@ -1,12 +1,5 @@
 import logging
 
-
-is_debug = False
-warning = None
-error = None
-info = None
-
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -26,62 +19,3 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
 info_logger = logging.getLogger("Info")
-
-def debug_decorator(func):
-    """
-    Decorator for printing info about function.
-
-    :param func: function that need decorating.
-    :func type: function.
-    :returns: decorated function.
-    :rtype: function.
-    """
-    global is_debug
-
-    return_func = None
-
-    if is_debug:
-    
-        def func_wrapper(msg, *args, **kwargs):
-
-            logger.info(f'Called from function: "{func.__name__}"')
-            logger.info(f"Arguments {msg, args, kwargs}")
-            func(msg, *args, **kwargs)
-        
-        return_func = func_wrapper
-    
-    else:
-        
-        return_func = func
-
-    return return_func
-
-
-def toggle_debug():
-    """
-    Toggles debug setting.
-
-    :returns: None.
-    :rtype: None.
-    """
-    global is_debug
-
-    is_debug = not is_debug
-
-    create_decorators()
-
-
-def create_decorators():
-    """
-    Creates decorators with new setting.
-
-    :returns: None.
-    :rtype: None.
-    """
-    global warning
-    global info
-    global error
-
-    warning = debug_decorator(logger.info)
-    info = debug_decorator(logger.info)
-    error = debug_decorator(logger.error)
--- Yurii_Smazhnyi/3/DungeonGame/main.py
+++ Yurii_Smazhnyi/3/DungeonGame/main.py
@@ -3,7 +3,6 @@ import dungeon_input
 import custom_log
 import serializer
 
-
 is_trap_near_player = False
 is_treasure_near_player = False
 game_map = []
@@ -125,30 +124,25 @@ def start():
 
     while True:
 
-        debug_status = "Off" if custom_log.is_debug else "On"
-        string = input(f"Start Game(1)/Load Game(2)/Exit Game(3)/Turn { debug_status } Debug(4):")
+        string = input("Start Game(1)/Load Game(2)/Exit Game(3) :")
 
         if string == '1':
 
             game_map = map_generator.create_map(10)
-            custom_log.info("Game Started!")
+            custom_log.logger.info("Game Started!")
             play()
 
         elif string == '2':
             game_map = serializer.load_map()
 
             if game_map == "":
-                custom_log.error("Failed to Load Game")
+                custom_log.logger.error("Failed to Load Game")
                 continue
 
-            custom_log.info("Game Started!")
+            custom_log.logger.info("Game Started!")
             play()
-
         elif string == '3':
             break
-
-        elif string == '4':
-            custom_log.toggle_debug()
 
 
 def print_map():
@@ -162,7 +156,7 @@ def print_map():
     global game_map
 
     for i in range(len(game_map)):
-        custom_log.info((game_map[i]))
+        print((game_map[i]))
 
 
 def check_status():
@@ -250,7 +244,7 @@ def play():
     global is_trap_near_player
     global game_map
 
-    custom_log.info("---------------------------------------------------")
+    custom_log.logger.info("---------------------------------------------------")
 
     while True:
 
@@ -258,13 +252,13 @@ def play():
 
         valid_direction = get_valid_directions()
 
-        custom_log.info("Input 'save'/'load' to save/load the game.")
-        custom_log.info(f"Valid directions - {valid_direction}")
+        custom_log.logger.info("Input 'save'/'load' to save/load the game.")
+        custom_log.logger.info(f"Valid directions - {valid_direction}")
 
         check_status()
 
-        custom_log.info(f"Traps Near You - {is_trap_near_player}")
-        custom_log.info(f"Treasure Near You - {is_treasure_near_player}")
+        custom_log.logger.info(f"Traps Near You - {is_trap_near_player}")
+        custom_log.logger.info(f"Treasure Near You - {is_treasure_near_player}")
 
         direction = dungeon_input.get_direction()
 
@@ -276,7 +270,7 @@ def play():
             new_game_map = serializer.load_map()
 
             if new_game_map == "":
-                custom_log.error("Failed to Load Game")
+                custom_log.logger.error("Failed to Load Game")
                 continue
 
             game_map = new_game_map
@@ -284,12 +278,12 @@ def play():
 
 
         if direction not in valid_direction:
-            custom_log.warning("Can't move there!")
+            custom_log.logger.warning("Can't move there!")
             continue
 
         item_stepped_on = move_player(direction)
 
-        custom_log.info("---------------------------------------------------")
+        custom_log.logger.info("---------------------------------------------------")
 
         if item_stepped_on == map_generator.TRAP_SYMBOL:
             end_game()
@@ -306,7 +300,7 @@ def end_game():
     :returns: None.
     :rtype: None.
     """
-    custom_log.info("You Lost!")
+    custom_log.logger.info("You Lost!")
     print_map()
 
 
@@ -317,7 +311,7 @@ def win_game():
     :returns: None.
     :rtype: None.
     """
-    custom_log.info("You Won!")
+    custom_log.logger.info("You Won!")
     print_map()
 
 
