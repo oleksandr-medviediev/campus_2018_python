017323781275464a4770721437c4bd1ff4ab825d
vbiletskyi <vbiletskyi@KIV-WKS-PR316>
2018-11-20 15:26:40 +0200

    Dungeon Game (just copied sources to separate folder for the next task).

---
+++ Vadym_Bilestkyi/6/map_generator.py
@@ -0,0 +1,21 @@
+from dungeon_logging import logger
+import random
+
+
+def generate(map_size):
+    new_map = [[None] * map_size for _ in range(map_size)]
+    logger.debug('Empty map generated')
+
+    for i in range(map_size**2 // 10):
+        x = random.choice(range(map_size))
+        y = random.choice(range(map_size))
+        new_map[x][y] = 'Trap'
+    logger.debug('Traps was placed')
+
+    for i in range(map_size**2 // 20):
+        x = random.choice(range(map_size))
+        y = random.choice(range(map_size))
+        new_map[x][y] = 'Treasure'
+    logger.debug('Treasures was placed')
+
+    return new_map
