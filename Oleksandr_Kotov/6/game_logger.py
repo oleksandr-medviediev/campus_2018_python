53d02b758079fc379fb0efcdb298a4ab8eb1e1fe
okotov <oleksandr.kotov@ubisoft.com>
2018-11-20 17:27:31 +0200

    batch 6

---
+++ Oleksandr_Kotov/6/game_logger.py
@@ -0,0 +1,46 @@
+"""module provides logging functions for the game
+"""
+
+import logging
+from decorators import debug_decorator
+from decorators import info_decorator
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+_message_handler = logging.StreamHandler()
+_message_handler.setLevel(logging.INFO)
+
+logger.addHandler(_message_handler)
+
+_file_handler = logging.FileHandler("log.txt", mode='w')
+_file_handler.setLevel(logging.DEBUG)
+
+_FILE_FORMAT = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+_file_handler.setFormatter(_FILE_FORMAT)
+
+logger.addHandler(_file_handler)
+
+
+@info_decorator
+@debug_decorator
+def log_debug(message):
+    """log message with severity DEBUG
+    
+    Arguments:
+        message str -- message to log
+    """
+
+    logger.debug(message)
+
+
+@info_decorator
+@debug_decorator
+def log_info(message):
+    """log message with severity INFO
+    
+    Arguments:
+        message str -- message to log
+    """
+
+    logger.info(message)
