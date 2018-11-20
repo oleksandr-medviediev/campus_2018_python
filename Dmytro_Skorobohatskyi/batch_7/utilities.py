4e5318b0b8d7fe4392873e3429b822bf98a89aa2
Dima Skorobogatskii <skorobogatskiidima@gmail.com>
2018-11-20 02:42:44 +0200

    Changed into OOP style by Skorobohatskyi

---
+++ Dmytro_Skorobohatskyi/batch_7/utilities.py
@@ -0,0 +1,20 @@
+import constants
+
+class Utilities:
+
+    @staticmethod
+    def get_shift_by_direction(direction_index):
+
+        """ Function return shift depending on direction.
+
+            Args:
+                direction_index(int): index specified direction
+
+            Returns:
+                (int, int): shift by columns and rows
+        """
+        
+        shift_x = constants.all_shifts[direction_index]['x']
+        shift_y = constants.all_shifts[direction_index]['y']
+
+        return shift_x, shift_y
