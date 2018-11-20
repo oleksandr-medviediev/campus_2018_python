79ddf1fbbfee72b1f374dc18bc328f4927da4576
knifantiev <kostiantyn.nifantiev@ubisoft.com>
2018-11-20 11:14:52 +0200

    Dungeon game with save/load and logging

---
+++ Kostiantyn_Nifantiev/5/Dungeon_GameV2/Dungeon_GameV2.py
@@ -0,0 +1,24 @@
+from game_functions import game_loop
+from dungeon_logger import my_logger
+
+
+while True:
+
+    my_logger.debug("Entering main game function")
+
+    game_loop()
+
+    my_logger.debug("Main game function has finished")
+
+    decision = input('Do you want to play again? y/n:\n')
+
+    if decision == 'y':
+
+        my_logger.debug("Restarting game")
+        continue
+
+    else:
+
+        my_logger.debug("Abandoning game")
+        break
+
