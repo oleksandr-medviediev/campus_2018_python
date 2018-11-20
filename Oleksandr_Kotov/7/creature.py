9b23a059cdb85e1f9eab203dca27da99050f3030
okotov <oleksandr.kotov@ubisoft.com>
2018-11-20 20:43:22 +0200

    batch 7 WIP

---
+++ Oleksandr_Kotov/7/creature.py
@@ -0,0 +1,55 @@
+
+import decorators
+
+
+class Creature:
+    """defines basic live game creature
+    """
+
+    __health = 0
+    __curr_row = 0
+    __curr_col = 0
+
+    @decorators.debug_decorator
+    def __init__(self,  row=0, col=0, health=0):
+        """init object with position
+
+        Keyword Arguments:
+            row {int} -- row on game map (default: {0})
+            col {int} -- column on game map (default: {0})
+            health {int} -- creature health (default: {0})
+
+        """
+        self.__health = health
+        self.set_position(row, col)
+
+    @property
+    def position(self):
+        return self.__curr_row, self.__curr_col
+
+    def set_position(self, row, col):
+        """set object position
+
+        Arguments:
+            row {int} -- row on game map
+            col {int} -- column on game map
+        """
+
+        self.__curr_row = row
+        self.__curr_col = col
+
+    @decorators.debug_decorator
+    def damage(self, damage):
+        """deal damage to creature
+
+        Arguments:
+            damage {int} -- number of points to decrement from health
+        """
+
+        self.__health -= damage
+
+    @property
+    @decorators.debug_decorator
+    def health(self):
+        return self.__health
+        
