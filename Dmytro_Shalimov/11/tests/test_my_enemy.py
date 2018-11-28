0967d1c543f6184ec51d77c2a894feeb398a0355
Shalim23 <31928965+Shalim23@users.noreply.github.com>
2018-11-28 18:43:30 +0200

    updated 11.1
    updated 11.1

---
+++ Dmytro_Shalimov/11/tests/test_my_enemy.py
@@ -0,0 +1,34 @@
+import unittest
+import os,sys,inspect
+
+sys.path.insert(0, '..') 
+
+import my_enemy
+
+
+class MyEnemyTest(unittest.TestCase):
+
+    def setUp(self):
+        
+        self.enemy_instance = my_enemy.Enemy()
+        self.map_size = 5
+        self.enemy_instance.initialize((3,2))
+
+
+    def test_is_new_position_generated(self):
+
+        _enemy_prev_position = self.enemy_instance.position
+
+        self.enemy_instance.position = self.enemy_instance._Enemy__generate_next_position(self.map_size)
+
+        _enemy_generated_position = self.enemy_instance.position
+
+        self.assertNotEqual(_enemy_generated_position, _enemy_prev_position, 'New position is not generated')
+
+
+    def tearDown(self):
+        print('Exiting enemy tests')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Dmytro_Shalimov/11/tests/test_my_map.py
@@ -0,0 +1,32 @@
+import unittest
+import os,sys,inspect
+
+sys.path.insert(0, '..') 
+
+import my_map
+
+
+class MyMapTests(unittest.TestCase):
+
+    def setUp(self):
+
+       self.map_instance = my_map.MyMap()
+       self.map_size_input = int(input('Enter map size: '))
+
+    def test_generated_map_size_equals_input_map_size(self):
+
+       self.map_instance.initialize(self.map_size_input)
+       _elements = 0
+
+       for row in self.map_instance.generated_map:
+           _elements += len(row)
+       
+       self.assertEqual(_elements / len(self.map_instance.generated_map), self.map_size_input,
+                       'generated map size is not correct')
+
+    def tearDown(self):
+        print('Exiting map tests')
+
+
+if __name__ == '__main__':
+    unittest.main()
