3baa06e7d86aa0d3906c06591f28765aec27cef6
Oleksandr Kotov <oleksandr.kotov@ubisoft.com>
2018-11-30 15:19:38 +0200

    created game package

---
+++ Oleksandr_Kotov/10/game_package_okotov/game_package_okotov/__init__.py
@@ -0,0 +1,3 @@
+name = "game_package_okotov"
+
+#https://pypi.org/project/game-package-okotov/#description
---
+++ Oleksandr_Kotov/10/game_package_okotov/game_package_okotov/decorators.py
@@ -0,0 +1,48 @@
+"""module provides decorators for the game
+"""
+from functools import wraps
+
+import game_logger
+
+
+def info_decorator(func):
+    """decorate a function into info wrapper
+
+    Arguments:
+        func callable -- function to wrap
+
+    Returns:
+        callable -- wrapped function
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def debug_decorator(func):
+    """decorates a function into debug wrapper
+    debug wrapper prints function name and arguments before call
+
+    Arguments:
+        func callable -- function to wrap
+
+    Returns:
+        callable -- wrapped function
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        game_logger.logging.debug("Function name: " + func.__name__)
+
+        game_logger.logging.debug("Args: ")
+        game_logger.logging.debug(args)
+
+        game_logger.logging.debug("Kwargs: ")
+        game_logger.logging.debug(kwargs)
+
+        return func(*args, **kwargs)
+
+    return wrapper
