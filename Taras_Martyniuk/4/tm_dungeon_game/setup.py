3ba34469c4402389b75d7112dfc047095e331682
Taras Martyniuk <tarasmartynyuk22@gmail.com>
2018-11-30 16:21:49 +0200

    long description

--- Taras_Martyniuk/4/tm_dungeon_game/setup.py
+++ Taras_Martyniuk/4/tm_dungeon_game/setup.py
@@ -7,5 +7,8 @@ setuptools.setup(
     name="tm_dungeon_game",
     version="0.0.1",
     author="Taras Martyniuk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TarasMartynyuk/campus_2018_python",
 )
 
