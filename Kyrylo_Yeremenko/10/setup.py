1b4a8eecf4fa0947380b2838a528c076ea486fd2
erem2k <erem275@gmail.com>
2018-12-01 19:52:28 +0200

    Fix - Fixed package name

--- Kyrylo_Yeremenko/10/setup.py
+++ Kyrylo_Yeremenko/10/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dungeon_game_",
+    name="dungeon_game_yeremenko",
     version="1.0.0",
     author="Kirill Yeremenko",
     author_email="erem275@gmail.com",
