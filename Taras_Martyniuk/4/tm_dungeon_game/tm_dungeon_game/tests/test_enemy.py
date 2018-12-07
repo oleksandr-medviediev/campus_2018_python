09744dc088125b3efe0b3bcb513655ed0af1c2a8
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-12-08 00:12:17 +0200

    move rand in bounds test

--- Taras_Martyniuk/4/tm_dungeon_game/tm_dungeon_game/tests/test_enemy.py
+++ Taras_Martyniuk/4/tm_dungeon_game/tm_dungeon_game/tests/test_enemy.py
@@ -7,21 +7,27 @@ class TestEnemy(utest.TestCase):
     RUNS = 10
 
 
+    def setUp(self):
+        self.dmap = DungeonMap(10)
+        self.enemy = Enemy(self.dmap)
+
+
     def test_move_randomly_moves_one_tile_only(self):
         def single_run():
-            dmap = DungeonMap(10)
-            en = Enemy(dmap)
-
-            start_pos = en.position
-            en.move_randomly()
-            diff = tuple_diff(en.position, start_pos)
-
-            print(f'st {start_pos}')
-            print(f'mv {en.position}')
-            print(f'df {diff}')
+            start_pos = self.enemy.position
+            self.enemy.move_randomly()
+            diff = tuple_diff(self.enemy.position, start_pos)
 
             valid_diffs = { -1, 1, 0 }
             assert diff[0] in valid_diffs and diff[1] in valid_diffs
 
         repeat(single_run, self.RUNS)
+
+    
+    def test_move_randomly_moves_in_bounds(self):
+        def single_run():
+            self.enemy.position = (0, 0)
+            self.enemy.move_randomly()
+            self.assertTrue(self.dmap.in_bounds(self.enemy.position)) 
+
 
